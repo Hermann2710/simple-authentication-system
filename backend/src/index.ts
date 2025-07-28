6db21b000adda4a6c82978ca2adda7f4633e870a
Hermann2710 <138356589+Hermann2710@users.noreply.github.com>
2025-07-28 22:52:26 +0100

    03 - Creation et intégration des routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,13 +2,18 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 import cors from "cors"
+import authRoutes from "./routes/auth-routes"
 
 dotenv.config()
 
 const app = express()
 
+// Middlewares
 app.use(cors())
 app.use(express.json())
+
+// Routes
+app.use("/api/auth", authRoutes)
 
 const PORT = process.env.PORT || 5000
 
